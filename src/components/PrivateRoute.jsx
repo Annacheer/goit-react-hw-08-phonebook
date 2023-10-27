a8c86b927009a12ad9e958e3ce6c0b57ace1de3b
Annacheer <125766262+Annacheer@users.noreply.github.com>
2023-10-27 09:59:59 -0400

    sixth commit

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -6,6 +6,6 @@ const PrivateRoute = ({ component: Component, redirectTo = '/' }) => {
   const { isLoggedIn, isRefreshing } = useAuth();
   const shouldRedirect = !isLoggedIn && !isRefreshing;
 
-  return shouldRedirect ? <Navigate to={redirectTo} /> : <Component />;
+  return shouldRedirect ? <Navigate to={redirectTo} /> : Component;
 };
 export default PrivateRoute;
